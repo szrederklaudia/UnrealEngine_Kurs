b92e0d13426ea851b0b1121244f3f2f605855a2b
eurydyca <szredi@o2.pl>
2019-12-07 20:22:54 +0100

    powitanie
    dodano "can u guess the 9 letter isogram..."

--- Sekcja_02/BullCowGame/Main.cpp
+++ Sekcja_02/BullCowGame/Main.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
 int main() 
 {
+	constexpr int WORLD_LENGTH = 9;
+
+	cout << "Welcome to Bulls and Cows, a fun word game" << endl;
+	cout << "Can you guess the " << WORLD_LENGTH;
+	cout << " letter isogram I'm thinking off?\n";
+
 	return 0;
 }
