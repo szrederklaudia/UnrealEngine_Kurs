46476bbba252808dec8568863fb1b42347b11b3b
eurydyca <szredi@o2.pl>
2019-12-15 22:29:31 +0100

    get guess and print back

--- Sekcja_02/BullCowGame/Main.cpp
+++ Sekcja_02/BullCowGame/Main.cpp
@@ -3,29 +3,38 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+// the entry point for our application
 int main() 
 {
-	//introduce the game
-	constexpr int WORLD_LENGTH = 9;
+	PrintIntro();
 
+	GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
+
+	cout << endl; 
+	return 0;
+}
+
+//introduce the game
+void PrintIntro() {
+	constexpr int WORLD_LENGTH = 9;
 	cout << "Welcome to Bulls and Cows, a fun word game" << endl;
 	cout << "Can you guess the " << WORLD_LENGTH;
 	cout << " letter isogram I'm thinking off?\n";
 	cout << endl;
+	return;
+}
 
-	//get a gues from the player
+//get a gues from the player
+string GetGuessAndPrintBack() {
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-	//repeat the guess back to them
-	cout << "Your guess was: " << Guess << endl;
 
-	//get a gues from the player
-	cout << "Enter your guess: ";
-	getline(cin, Guess);
-	//repeat the guess back to them
+	//print the guess back
 	cout << "Your guess was: " << Guess << endl;
-
-	cout << endl; 
-	return 0;
+	return Guess; 
 }
